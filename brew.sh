e1c7e954f0c6b1a37566a44f9ae6e686d6008dd7
Philokane <Philokane@users.noreply.github.com>
2019-09-20 13:09:18 +0100

    update brew.sh

---
+++ brew.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# chmod a+x brew.sh
+# https://computers.tutsplus.com/tutorials/perfect-configurations-with-homebrew-and-cask--cms-20768
+
+# brew packages
+brew install git
+brew install gradle
+brew install openssl
+brew install ruby
+
+# brew cask
+# https://caskroom.github.io/search
+brew cask install 1password
+# brew cask install adium
+brew cask install android-file-transfer
+brew cask install anaconda
+brew cask install alfred
+brew cask install aton
+# brew cask install appcleaner
+# brew cask install appcode
+brew cask install authy
+brew cask install charles
+# brew cask install clion
+# brew cask install cronnix
+brew cask install dash
+brew cask install discord
+brew cask install docker
+brew cask install dropbox
+brew cask install firefox
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install daisydisk
+brew cask install istat-menus
+brew cask install iterm2
+# brew cask install keepingyouawake
+brew cask install mailplane
+brew cask install mysql
+# brew cask install postgres
+brew cask install postman
+brew cask install pycharm
+brew install python
+brew install python3
+brew cask install spotify
+brew cask install signal
+# brew cask install razorsql
+brew cask install skitch
+brew cask install slack
+brew cask install the-unarchiver
+brew cask install telegram
+brew cask install transmit
+brew cask install p7zip
+brew cask install tower #git client
+brew cask install unrarx
+brew cask install virtualbox
+brew cask install visual-studio-co
+brew cask install vlc
+brew cask install whatsapp
+brew cask install wireshark
+
